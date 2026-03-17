9737cec08580a4be42bca560dbabd9a6f0c65041
Seigi Gim <seigi.fgim@gmail.com>
2026-03-16 21:01:34 -0300

    feat: implement signals for product management and enhance product retrieval methods

--- src/app/features/products/services/products.service.ts
+++ src/app/features/products/services/products.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { Product } from '../interfaces/product.interface';
 import { environment } from '../../../../environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 interface Options {
   limit?: number;
@@ -16,7 +16,12 @@ interface Options {
 export class ProductsService {
   private http = inject(HttpClient);
   private readonly baseUrl = environment.baseUrl;
-
+  private _products = signal<Product[]>([]);
+  private _product = signal<Product | null>(null);
+  private _productsByCategory = signal<Product[]>([]);
+  readonly products = this._products.asReadonly();
+  readonly product = this._product.asReadonly();
+  readonly productsByCategory = this._productsByCategory.asReadonly();
 
   getProducts(options: Options): Observable<Product[]> {
     const { limit = 9, offset = 0, gender = '' } = options;
@@ -28,4 +33,23 @@ export class ProductsService {
       }
     });
   }
+
+  getProductById(id: string): Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/products/${id}`).pipe(
+      tap(product => this._product.set(product))
+    )
+  }
+
+  getProductsByCategoryId(categoryId: string): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/products/?categoryId=${categoryId}`).pipe(
+      tap(products => this._productsByCategory.set(products))
+    )
+  }
+
+  getProductsByCategorySlug(categorySlug: string): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/products/?categorySlug=${categorySlug}`).pipe(
+      tap(products => this._productsByCategory.set(products))
+    )
+  }
 }
+
